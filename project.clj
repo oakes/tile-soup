3ce90d61d6864cb7f3932e51b0bef3ff1ba5699e
oakes <zach@sekao.net>
2020-09-12 09:32:27 -0400

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tile-soup "0.4.2-SNAPSHOT"
+(defproject tile-soup "0.4.2"
   :description "A library to parse tiled maps"
   :url "https://github.com/oakes/tile-soup"
   :license {:name "Public Domain"
